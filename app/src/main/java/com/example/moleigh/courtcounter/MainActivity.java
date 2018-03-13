747ed1190b8285f36281379ebe36ee53f62981bf
MoLeighJackson <moleighjackson@gmail.com>
2018-03-12 22:43:24 -0400

    In Java, set up methods for points.

--- app/src/main/java/com/example/moleigh/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/moleigh/courtcounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.moleigh.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(0);
+    }
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Add points for Team A.
+     */
+    public void addThreePointsForTeamA(View v) {
+        displayForTeamA(3);
+    }
+
+    /**
+     * Add points for Team A.
+     */
+    public void addTwoPointsForTeamA(View v) {
+        displayForTeamA(3);
+    }
+
+    /**
+     * Add points for Team A.
+     */
+    public void addOnePointForTeamA(View v) {
+        displayForTeamA(3);
     }
 }
